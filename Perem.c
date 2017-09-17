c90ba0249629c1014ae41a74e6631be360ed9df0
Anton Khebnev <Khebnev.a.v@gmail.com>
2017-09-17 12:16:23 +0300

    Added examples with perem

--- Perem.c
+++ Perem.c
@@ -1,9 +1,17 @@
 #include <stdio.h>
-int main
+int main()
 {
 	int MyVar = 30;
 	int Number; // Не инициализована
 	Number = 50;
 	int var, perem, folder; // тип данных int 
 	var = 987;
+	char letter = 'A';
+	int number1 = 88;
+	int number2, number3;
+	number2 = 45;
+	short MyNumber = 999;
+	float decimal = 2.5;
+	double decimal_double = 7.134589;
+	long long MyLong = 78945612313332148;
 }
