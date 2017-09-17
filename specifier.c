d177f8a1da9bc2ca291001518fbc4940822e95b6
Anton Khebnev <Khebnev.a.v@gmail.com>
2017-09-17 13:11:06 +0300

    Added examples with specifiers

---
+++ specifier.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+	char letter = 'A';
+	double decimal = 5.489;
+	int number = 100;
+	printf("%c - Одиночный символ\n",  letter);  //% c - спецификатор символьной переменной.
+	printf("decimal = %g, number = %d\n", decimal, number); // %d спецификатор ЦЕЛОГО числа, %g - спецификатор double или float.
+	printf("%05.2g - decimal\n", decimal); /*.2g -  точность 2 знака после запятой
+	%05. - 5 знаков до запятой, пустые разряды, 
+	если их нет - будут заполнены нолями.*/
+ }
