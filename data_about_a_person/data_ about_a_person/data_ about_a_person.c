6e2584bbfd21e12ff57944617c404462ccdcba1d
Anton Khebnev <Khebnev.a.v@gmail.com>
2017-09-17 21:19:20 +0300

    Added solution Escape

--- data_about_a_person/data_ about_a_person/data_ about_a_person.c
+++ data_about_a_person/data_ about_a_person/data_ about_a_person.c
@@ -1,19 +1,21 @@
 #include <stdio.h>
+#include <string.h> 
+#include <stdlib.h>
 #include <locale.h>
 
 int main()
 {
 	setlocale(LC_ALL, "rus");
 	int age, weight;
-	char name[90];  //Объевляем массив символов из 50шт.
+	char name[30];  //Объевляем массив символов из 50шт.
 	printf("Введите Ваше имя: ");
 	scanf_s("%s", name);/*Спецификатор строки %s. &name - адрес, куда будет сохраняться строка, т.е. в переменную name.
 					  Также,  символ & перед строковыми типами данных ставить не нужно. С остальными - ставим.*/
 	printf("Введите Ваш возраст: ");
 	scanf_s("%d", &age);
 	printf("Введите Ваш вес: ");
-	scanf_s("%d", &weight); 
+	scanf_s("%d", &weight);
 	printf("Выведем нашу информацию:\n");
-	printf_s("Ваше имя - %s \n", name);
-	printf("Ваш возраст - %d лет\nВаш вес - %d кг \n", age, weight);
-}
+	printf("Вы ввели следующую строку: %s", name );
+	printf("Ваш возраст - %d лет\nВаш вес - %d кг \n   И ещё раз твой возраст - %d", age, weight, age);
+}  
